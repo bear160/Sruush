29c2d854e6b2b7f5a55678ceede5b145f86f8634
bear160 <isruuuuuuush@gmail.com>
2025-03-22 17:49:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    let message = document.getElementById("hiddenMessage");
+    message.innerHTML = "You are my forever love, Salim! 💖💋";
+    message.style.display = "block";
+}
